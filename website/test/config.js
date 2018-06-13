5b4bf87ecab1dcb03aa3cd3e7896bc3293f27165
root <root@AX206S16.local.server>
2018-06-14 01:29:33 +0800

    Commit-2018-06-14

---
+++ website/test/config.js
@@ -0,0 +1,13 @@
+var api = "https://pool.tvvpro.com:6218";
+
+var email = "support@tvvpro.com";
+var telegram = "";
+var discord = "";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "http://explorer.tvvpro.com/?hash={id}#blockchain_block";
+var transactionExplorer = "http://explorer.tvvpro.com/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default.css";
+var defaultLang = 'en';
